29a9f203bec3d54cf9281d8e8e888e93d50f29c6
howItPhills <vlad-man001@mail.ru>
2022-02-25 09:52:13 +0300

    made header component using material ui

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { Autocomplete } from '@react-google-maps/api'
+import { Typography, AppBar, Toolbar, Box, InputBase } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search';
+
+import useStyles from './styles';
+
+//Typography - text element (h1-h6, title, subtitle); Box - div element;
+//AppBar - header element; Toolbar - navigation bar;
+
 
 const Header = () => {
+
+   const classes = useStyles()
+
    return (
-      <div>Header</div>
+      <AppBar position='static'>
+         <Toolbar className={classes.toolbar}>
+            <Typography variant='h5' className={classes.title}>
+               Travel Advisor
+            </Typography>
+            <Box display='flex'>
+               <Typography variant='h6' className={classes.title}>
+                  Explore new places
+               </Typography>
+               {/* <Autocomplete> */}
+               <div className={classes.search}>
+                  <div className={classes.searchIcon}>
+                     <SearchIcon />
+                  </div>
+                  <InputBase placeholder='Search...' classes={{ root: classes.input, input: classes.inputInput }} />
+               </div>
+               {/* </Autocomplete> */}
+            </Box>
+         </Toolbar>
+      </AppBar>
    )
 }
 
